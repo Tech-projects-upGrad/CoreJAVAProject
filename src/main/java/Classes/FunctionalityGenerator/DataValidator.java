e1632b30c5f6c4f7793f4a2b8448cf43f231ea74
sakutars123 <contact.satyamkunal@gmail.com>
2021-12-11 16:16:54 +0530

    Commit Initial - Feature class Card Added.

---
+++ src/main/java/Classes/FunctionalityGenerator/DataValidator.java
@@ -0,0 +1,14 @@
+package Classes.FunctionalityGenerator;
+
+import java.util.HashMap;
+
+public class DataValidator {
+
+    //Todo: logic for checking the hashmap is correct before input it in DB.
+    public static boolean DataValidationHashMap(HashMap<Integer, HashMap<String,String >> map){
+
+
+        return true;
+    }
+
+}
